251886738da77da281a088e1def1ae654ec5291f
Riccardo Perello <rperello@gio.lu>
2021-11-16 11:34:11 +0100

    js preventDefault

--- js/main.js
+++ js/main.js
@@ -1,26 +1,19 @@
-function open(overlay) {
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${window.scrollY}px`;
+let previousPosition
 
-
+function open(overlay, event) {
+    event.preventDefault();
     overlay.classList.add('open');
-
 }
 
 function close(overlay) {
-    const scrollY = document.body.style.top;
-    document.body.style.position = '';
-    document.body.style.top = '';
-    window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    console.log(scrollY);
-
     overlay.classList.remove('open');
 }
 
 
 // Define event handler
 const onPageLoad = (e) => {
-    setTimeout(function() { window.scrollTo(0, 1) }, 100);
+
+    // setTimeout(function() { window.scrollTo(0, 1) }, 100);
 
     const overlay = document.getElementById('overlay');
     const closeOverlay = document.querySelectorAll('.close-overlay');
